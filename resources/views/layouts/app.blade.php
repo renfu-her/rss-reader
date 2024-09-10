778e684f5ff6e9d38d6c99c92c23a895428b15d8
Patrick Her <renfu.her@gmail.com>
2024-09-10 15:36:17 +0800

    chore: Remove unnecessary script tag in app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,7 +28,6 @@
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/lazyload@2.0.0-rc.2/lazyload.js"></script>
 
-
     @stack('scripts')
 </body>
 
